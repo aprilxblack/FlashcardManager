f13a3ae729d3b162bdfc25b5534baf882b5fc0d6
Karolina Zdziarska <Karolina.Zdziarska@britishland.com>
2020-05-04 00:40:22 +0100

    Added popup for Continue Learning button

--- FlashcardManager/ClientApp/src/components/Home.js
+++ FlashcardManager/ClientApp/src/components/Home.js
@@ -10,16 +10,44 @@ export class Home extends Component {
         super(props);
         this.lastOpenedSet = this.props.lastOpenedSet;
     }
-    handleClick() {
-        console.log('click');
+    openModal() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+    }
+    closeModal() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "none";
     }
   render () {
     return (
       <div>
             <h4 className="mb-3">Hi Username! What would you like to do today?</h4>
-            <ActionBox displayText="Continue Learning" onClick={this.handleClick} action="#" icon={ContinueIcon} />
+
+            {this.lastOpenedSet == null ? <><ActionBox displayText="Continue Learning" onClick={this.openModal} action="#" icon={ContinueIcon} /> </> :
+                <> <ActionBox displayText="Continue Learning" action={"/learn/" + this.lastOpenedSet} icon={ContinueIcon} /> </>}
             <ActionBox displayText="Browse Sets" action="/counter" icon={SearchIcon} />
             <ActionBox displayText="Create a Set" action="/counter" icon={PlusIcon} /> 
+
+            <div id="myModal" class="modal">
+
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel"></h5>
+                            <button type="button" class="close" onClick={this.closeModal} aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            You haven't opened any sets recently. Please select Browse Sets option instead.
+                         </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" onClick={this.closeModal}>OK</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
       </div>
     );
   }
